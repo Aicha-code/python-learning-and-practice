4baa710d747a8836b9d1c4cfc0d5d307ec1e79ff
Abari Aichetou <ug2321291@ines.ac.rw>
2025-09-29 15:04:48 +0200

    feat: add multiplication and division support

--- arithmetic-operations-formatter.py
+++ arithmetic-operations-formatter.py
@@ -1,10 +1,10 @@
 def main():
     print("Welcome to the Arithmetic Formatter!\n")
-    print("You can enter up to five arithmetic problems involving addition or subtraction.")
+    print("You can enter up to five arithmetic problems involving '+', '-', '/' or '*'.")
     print("Each problem should be in the format: 'operand1 operator operand2' (e.g., '32 + 698').")
     operation= input("Enter the arithmetic problems separated by commas as in the example above: ")
     problems = [problem.strip() for problem in operation.split(",")]
-    show_answers = input("Do you want to display the answers? (yes/no): ").strip().lower() == 'yes'
+    show_answers = input("Do you want to display the answers? (y/n): ").strip().lower() == 'y'
     arranged_problems = arithmetic_arranger(problems, show_answers)
     print(arranged_problems)
 
@@ -30,18 +30,23 @@ def arithmetic_arranger(problems, show_answers=False):
                 return "Error: Numbers must only contain digits."
             if len(first_operand) > 4 or len(second_operand)>4:
                 return 'Error: Numbers cannot be more than four digits.'
-
-            if operator not in ['+', '-']:
-                return "Error: Operator must be '+' or '-'."
+            
             width = max(len(first_operand), len(second_operand)) + 2
             lines.append("-" * width)
             first_operands.append(first_operand.rjust(width))
             operators.append(operator + second_operand.rjust(width - 1))
+            if operator not in ['+', '-', '*', '/']:
+                return "Error: Operator must be '+', '-', '*' or '/'."
             if show_answers:
                 if operator == '+':
                     result= str(int(first_operand) + int(second_operand))
-                else:
+                elif operator == '-':
                     result = str(int(first_operand) - int(second_operand))
+                elif operator== '*':
+                    result= str(int(first_operand) * int(second_operand))
+                elif operator== '/':
+                    result = f"{int(first_operand) / int(second_operand):.2f}"
+                
                 answers.append(result.rjust(width))
         arranged_first_line = "    ".join(first_operands)
         arranged_second_line = "    ".join(operators)
@@ -53,6 +58,8 @@ def arithmetic_arranger(problems, show_answers=False):
         return problem
     except IndexError:
             return "Error: Invalid problem format. Try again!"
+    except ZeroDivisionError:
+            return "Error: Division by zero is not allowed. Try something else!"
 
 if __name__ == "__main__":
     main()
